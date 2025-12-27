a52f3fcc7c6e6235cbbbed6d6cba68b480587f4f
ChunHajin <129915750+ChunHajin@users.noreply.github.com>
2025-12-28 02:36:41 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 빠른 입출력을 위한 설정
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string line;
+
+    while (getline(cin, line)) {
+        cout << line << "\n";
+    }
+
+    return 0;
+}
