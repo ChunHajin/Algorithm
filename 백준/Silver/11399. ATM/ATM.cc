ec1a023d2846ddfa6a596b859f7b342752c494b9
ChunHajin <129915750+ChunHajin@users.noreply.github.com>
2026-01-08 16:16:30 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    for (int i=0; i<N; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+    int P = 0;
+    for (int i=0; i<N; i++) {
+        P += v[i] * (N-i);
+    }
+    cout << P << "\n";
+
+    return 0;
+}
