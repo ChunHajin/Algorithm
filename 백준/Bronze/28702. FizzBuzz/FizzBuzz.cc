b29f2b821d516fad845609d19cb55d2d319d1d06
ChunHajin <129915750+ChunHajin@users.noreply.github.com>
2026-02-04 23:34:23 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -4,6 +4,7 @@ using namespace std;
 
 string s[3];
 int n[3] = {0, 0, 0};
+int ans;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -13,13 +14,8 @@ int main() {
         cin >> s[i];
         if (s[i][0] >= '0' && s[i][0] <= '9') {
             n[i] = stoi(s[i]);
-        }
-    }
-
-    int ans;
-    for (int i=0; i<3; i++) {
-        if (n[i] != 0) {
             ans = n[i] + (3-i);
+            break;
         }
     }
 
